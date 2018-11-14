035cf532f1ae6327958260d46305d644cc75b405
Polyakov Andrey <apolyakov@gtrf.ru>
2018-11-14 18:43:57 +0300

    dnd

--- src/dnd.js
+++ src/dnd.js
@@ -27,6 +27,21 @@ const homeworkContainer = document.querySelector('#homework-container');
    homeworkContainer.appendChild(newDiv);
  */
 function createDiv() {
+    function getRandom(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    const newDiv = document.createElement('div');
+
+    newDiv.style.height = getRandom(1, 600) + 'px';
+    newDiv.style.width = getRandom(1, 600) + 'px';
+    newDiv.style.backgroundColor = '#' + Math.random().toString(16).slice(-6);
+    newDiv.style.position = 'absolute';
+    newDiv.style.left = getRandom(1, 600) + 'px';
+    newDiv.style.top = getRandom(1, 600) + 'px';
+    newDiv.classList.add('draggable-div');
+
+    return newDiv;
 }
 
 /*
@@ -38,6 +53,23 @@ function createDiv() {
    addListeners(newDiv);
  */
 function addListeners(target) {
+    target.addEventListener('mousedown', (event) => {
+        let oX = event.pageX - target.getBoundingClientRect().left;
+        let oY = event.pageY - target.getBoundingClientRect().top;
+
+        let moveTo = (event) => {
+            target.style.left = event.pageX - oX + 'px';
+            target.style.top = event.pageY - oY + 'px';
+        };
+
+        let mouseUp = () => {
+            document.removeEventListener('mousemove', moveTo);
+            target.removeEventListener('mouseup', mouseUp);
+        };
+
+        document.addEventListener('mousemove', moveTo);
+        target.addEventListener('mouseup', mouseUp);
+    });
 }
 
 let addDivButton = homeworkContainer.querySelector('#addDiv');
